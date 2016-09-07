a80725429bb55e152aa74b07e141e3af69cca102
Oscar Odon Infante <oodon@inclusionservices.com>
2016-09-06 21:38:23 -0300

    Agrego los archivos de cosa-thingspeak que modificamos la ves pasada por si quieren seguir probando.

---
+++ cosaThingSpeak/README.md
@@ -0,0 +1,22 @@
+# Cosa-ThingSpeak
+
+This Cosa library is an implementation of the ThingSpeak client
+protocol; Channel update and TalkBack command handler.
+
+# Install
+
+To use this library you must download and install the [Arduino IDE] (http://www.arduino.cc/en/Main/Software) (or
+GCC AVR toolchain) and [Cosa] (https://github.com/mikaelpatel/Cosa).
+
+Download and unzip the Cosa-ThingSpeak library into your sketchbook
+libraries directory. Rename from Cosa-ThingSpeak-master to ThingSpeak.
+
+The ThingSpeak library and examples should be found in the Arduino IDE
+File>Sketchbook menu. Open the CosaThingSpeakClient example sketch. Select
+the Cosa core by selecting one of the Cosa boards in the Tools>Board
+menu.
+
+# References
+
+1. ThingSpeak API, http://community.thingspeak.com/documentation/api/
+
---
+++ cosaThingSpeak/examples/CosaThingSpeakChannel/CosaThingSpeakChannel.ino
@@ -0,0 +1,103 @@
+/**
+ * @file CosaThingSpeakChannel.ino
+ * @version 1.0
+ *
+ * @section License
+ * Copyright (C) 2014-2015, Mikael Patel
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * @section Description
+ * ThingSpeak channel demonstration; post temperature and humidity
+ * reading from DHT11 sensor to ThingSpeak server.
+ *
+ * @section Circuit
+ * This sketch is designed for the Ethernet Shield.
+ *
+ *                       W5100/ethernet
+ *                       +------------+
+ * (D10)--------------29-|CSN         |
+ * (D11)--------------28-|MOSI        |
+ * (D12)--------------27-|MISO        |
+ * (D13)--------------30-|SCK         |
+ * (D2)-----[ ]-------56-|IRQ         |
+ *                       +------------+
+ *
+ *                       DHT11/sensor
+ *                       +------------+
+ * (VCC)---------------1-|VCC  ====== |
+ * (D2/EXT0)-----------2-|DATA ====== |
+ *                     3-|     ====== |
+ * (GND)---------------4-|GND  ====== |
+ *                       +------------+
+ *
+ * This file is part of the Arduino Che Cosa project.
+ */
+
+// Uncomment to remove debug output
+// #define NDEBUG
+
+#include <DHT.h>
+#include <DHCP.h>
+#include <DNS.h>
+#include <W5X00.h>
+#include <W5100.h>
+// #include <W5200.h>
+#include <ThingSpeak.h>
+
+#include "Cosa/RTT.hh"
+#include "Cosa/Watchdog.hh"
+
+#ifndef NDEBUG
+#include "Cosa/Trace.hh"
+#include "Cosa/UART.hh"
+#else
+#define TRACE(x) x
+#endif
+
+// Digital humidity and temperature sensor
+#if defined(BOARD_ATMEGA2560)
+DHT11 sensor(Board::EXT4);
+#else
+DHT11 sensor(Board::EXT0);
+#endif
+
+// Ethernet controller
+static const char HOSTNAME[] __PROGMEM = "CosaThingSpeakChannel";
+W5100 ethernet;
+// W5200 ethernet;
+
+// Thingspeak client
+static const char KEY[] __PROGMEM = "I3U14KLWM1R1SDPR";
+ThingSpeak::Client client;
+
+void setup()
+{
+#ifndef NDEBUG
+  uart.begin(9600);
+  trace.begin(&uart, PSTR("CosaThingSpeakChannel: started"));
+#endif
+  Watchdog::begin();
+  RTT::begin();
+  TRACE(ethernet.begin_P(HOSTNAME));
+  TRACE(client.begin(ethernet.socket(Socket::TCP)));
+}
+
+void loop()
+{
+  ThingSpeak::Channel channel(&client, KEY);
+  ThingSpeak::Entry update;
+  sensor.sample();
+  update.set_field(1, sensor.temperature(), 1);
+  update.set_field(2, sensor.humidity(), 1);
+  TRACE(channel.post(update));
+  sleep(20);
+}
